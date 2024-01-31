82a0a21f951ea3bb01d175db1c180afe7cc4f307
briliyantoakbar <95515142+briliyantoakbar@users.noreply.github.com>
2024-01-31 14:39:57 +0700

    Update simpleOTA.py

--- simpleOTA.py
+++ simpleOTA.py
@@ -13,102 +13,16 @@ import sqlite3
 from pydantic import BaseModel
 global versi
 app = FastAPI()
-conn=sqlite3.connect("databaseOTA.db",check_same_thread=False)
-cursor=conn.cursor()
+
 class Item(BaseModel):
     name:list
 # Commit the changes and close the connection
 
-# Run the table creation at startup
-templates=Jinja2Templates(directory="htmldirectory")
-
-@app.get("/home/", response_class=HTMLResponse)  #Alamat link untuk memasukki menu home
-def home(request: Request):
-    return templates.TemplateResponse("index.html",{"request":request}) #menampilkan halaman HTML
-
-@app.post("/submitform")
-
-async def index2(versi:int=Form(...),file:UploadFile=File(...)): 
-    conn=sqlite3.connect("databaseOTA.db",check_same_thread=False)
-    cursor=conn.cursor()
-    file_content = await file.read()
-    file_name = file.filename
-    global versi2
-    versi2=versi
-    cursor.execute("INSERT INTO ota VALUES (?,?,?,?)",(None,versi,file_name,file_content))
-    conn.commit()
-    return {"aku":"oke"}
 
 
-# @app.get("/cat/{versi}") #Alamat link untuk mendownload file
-# def catd(versi):
-#     global data
-#     global datas
-#     conn=sqlite3.connect("databaseOTA.db",check_same_thread=False)
-#     cursor=conn.cursor()
-#     row=cursor.execute("SELECT id,versi, name,data from ota WHERE versi = ?", (str(versi)))
-#     image_data = row[2] # type: ignore
-#     # for row in cursor:
-#     #     c=True
-#     #     print("HAIII")
-#     #     print(row[0])
-#     #     print(row[1])
-#     #     print(row[2])
-#     #     data=row[1]
-#     temp_file_path = f"temp_{uuid.uuid4()}.jpg"  # Ganti ekstensi sesuai format gambar
-#     with open(temp_file_path, "wb") as file:
-#         file.write(image_data)
 
-#     return FileResponse(temp_file_path, media_type='text')
-    # return {"data":"oke"}
-        # return FileResponse(path=datas, media_type="text",filename=data)
-        # return StreamingResponse(io.BytesIO(datas), media_type="application/octet-stream")
 
-    
-@app.get("/p/{versiw}/y/{idr}")  #Alamat link untuk request version
-def index(versiw,idr):
-    url="https://fastapiskripsi-be199a487d88.herokuapp.com/c/"+versiw+idr
 
-    return {"version":versiw,"url":url}
-
-@app.get("/c/{versi}")  # Alamat link untuk mendownload file
-def catd(versi: str):
-    conn = sqlite3.connect("databaseOTA.db", check_same_thread=False)
-    cursor = conn.cursor()
-    
-    cursor.execute("SELECT data FROM ota WHERE versi = ?", (versi,))
-    row = cursor.fetchone()
-    conn.close()
-
-    if row is None:
-        raise HTTPException(status_code=404, detail="Version not found")
-
-    image_data = row[0]
-
-    temp_file_path = f"temp_{uuid.uuid4()}.jpg"  # Change extension based on actual image type
-    with open(temp_file_path, "wb") as file:
-        file.write(image_data)
-
-    response = FileResponse(temp_file_path, media_type='text')
-
-    # Cleanup temporary file after response
-    # @response.background
-    # def cleanup():
-    #     os.remove(temp_file_path)
-    return response
-@app.post("/tampilversi")
-def tampil_keluaran(payload: dict=Body(...)):
-    lis=[]
-    conn=sqlite3.connect("databaseOTA.db",check_same_thread=False)
-    cursor=conn.cursor()
-    getkontak=payload['name']
-    if(getkontak=="cekdata"):
-        data=conn.execute('SELECT versi FROM ota')
-        for i in data:
-            print(i)
-            lis.append(i[0])
-        print(lis)
-    return {"data":lis}
 
 
 @app.post("/edit")
